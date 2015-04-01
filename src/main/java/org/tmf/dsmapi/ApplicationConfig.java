5fbc43786d90f16b5fcff4c7cd8fcdd8fda6e56a
SIMON28 <eric.simon28@orange.fr>
2015-04-01 13:08:06 +0200

    maj page ui subscriber with new event, uri subscriber,applicationConfig

--- src/main/java/org/tmf/dsmapi/ApplicationConfig.java
+++ src/main/java/org/tmf/dsmapi/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package org.tmf.dsmapi;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -8,27 +9,24 @@ public class ApplicationConfig extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        return getRestResourceClasses();
-    }
-
-    private Set<Class<?>> getRestResourceClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
-        resources.add(org.tmf.dsmapi.commons.jaxrs.BadUsageExceptionMapper.class);
-        resources.add(org.tmf.dsmapi.commons.jaxrs.JacksonConfigurator.class);
-        resources.add(org.tmf.dsmapi.commons.jaxrs.JsonMappingExceptionMapper.class);
-        resources.add(org.tmf.dsmapi.commons.jaxrs.UnknowResourceExceptionMapper.class);
-        resources.add(org.tmf.dsmapi.hub.HubResource.class);
-        resources.add(org.tmf.dsmapi.individual.IndividualAdminResource.class);
-        resources.add(org.tmf.dsmapi.individual.IndividualResource.class);
-        resources.add(org.tmf.dsmapi.organization.OrganizationAdminResource.class);
-        resources.add(org.tmf.dsmapi.organization.OrganizationResource.class);
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(org.tmf.dsmapi.commons.jaxrs.BadUsageExceptionMapper.class);
+        classes.add(org.tmf.dsmapi.commons.jaxrs.JacksonConfigurator.class);
+        classes.add(org.tmf.dsmapi.commons.jaxrs.JsonMappingExceptionMapper.class);
+        classes.add(org.tmf.dsmapi.commons.jaxrs.UnknowResourceExceptionMapper.class);
+        classes.add(org.tmf.dsmapi.hub.HubResource.class);
+        classes.add(org.tmf.dsmapi.individual.IndividualAdminResource.class);
+        classes.add(org.tmf.dsmapi.individual.IndividualResource.class);
+        classes.add(org.tmf.dsmapi.organization.OrganizationAdminResource.class);
+        classes.add(org.tmf.dsmapi.organization.OrganizationResource.class);
         // following code can be used to customize Jersey 2.x JSON provider:
         try {
 			Class jacksonProvider = Class.forName("org.glassfish.jersey.jackson.JacksonFeature");
-			resources.add(jacksonProvider);
+			classes.add(jacksonProvider);
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(getClass().getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        return resources;
+        return classes;
     }
+
 }
